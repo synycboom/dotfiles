60e4fd0fe119b8712f043d3b532f6d6c40ed99a8
synycboom <synycboom@gmail.com>
2022-09-11 22:29:09 +0700

    feat: use zplug

--- install.ubuntu.sh
+++ install.ubuntu.sh
@@ -8,8 +8,8 @@ sudo apt -y install neovim
 # change default shell
 chsh -s $(which zsh)
 
-# install antigen
-curl -L git.io/antigen > $HOME/antigen.zsh
+# install zplug
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 # install fonts
 rm -rf /tmp/fonts && git clone https://github.com/powerline/fonts.git --depth=1 /tmp/fonts && /tmp/fonts/install.sh && rm -rf /tmp/fonts
