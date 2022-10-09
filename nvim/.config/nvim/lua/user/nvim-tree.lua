77fcf567242da3e37fc3f5f4cf176396329e5507
synycboom <synycboom@gmail.com>
2022-10-09 17:09:42 +0700

    feat: reconfigure nvim-tree

--- nvim/.config/nvim/lua/user/nvim-tree.lua
+++ nvim/.config/nvim/lua/user/nvim-tree.lua
@@ -3,43 +3,42 @@ if not status_ok then
   return
 end
 
--- disable netrw
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
 nvim_tree.setup {
-  -- update_focused_file = {
-  --   enable = true,
-  --   update_cwd = true,
-  -- },
-  -- renderer = {
-  --   root_folder_modifier = ":t",
-  --   icons = {
-  --     glyphs = {
-  --       default = "",
-  --       symlink = "",
-  --       folder = {
-  --         arrow_open = "",
-  --         arrow_closed = "",
-  --         default = "",
-  --         open = "",
-  --         empty = "",
-  --         empty_open = "",
-  --         symlink = "",
-  --         symlink_open = "",
-  --       },
-  --       git = {
-  --         unstaged = "",
-  --         staged = "S",
-  --         unmerged = "",
-  --         renamed = "➜",
-  --         untracked = "U",
-  --         deleted = "",
-  --         ignored = "◌",
-  --       },
-  --     },
-  --   },
-  -- },
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+  },
+  renderer = {
+    root_folder_modifier = ":t",
+    icons = {
+      glyphs = {
+        default = "",
+        symlink = "",
+        folder = {
+          arrow_open = "",
+          arrow_closed = "",
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+          symlink_open = "",
+        },
+        git = {
+          unstaged = "",
+          staged = "S",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "U",
+          deleted = "",
+          ignored = "◌",
+        },
+      },
+    },
+  },
   diagnostics = {
     enable = true,
     show_on_dirs = true,
@@ -59,5 +58,6 @@ nvim_tree.setup {
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 
-keymap("n", "<leader>t", ":NvimTreeToggle <CR>", opts)
+keymap("n", "<leader>tt", ":NvimTreeToggle <CR>", opts)
+keymap("n", "<leader>tf", ":NvimTreeFindFileToggle <CR>", opts)
 
