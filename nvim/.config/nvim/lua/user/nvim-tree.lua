431f57f2ff88cd198d700edbfa269d4cecb6f21e
synycboom <synycboom@gmail.com>
2023-11-22 11:18:08 +0700

    feat: change nvim-tree to floating window

--- nvim/.config/nvim/lua/user/nvim-tree.lua
+++ nvim/.config/nvim/lua/user/nvim-tree.lua
@@ -6,7 +6,30 @@ end
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
+local HEIGHT_RATIO = 0.8
+local WIDTH_RATIO = 0.5
+
+local function on_attach(bufnr)
+  local api = require "nvim-tree.api"
+
+  local function opts(desc)
+    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', '<Esc>', api.tree.close, opts('Close'))
+
+  -- Automatically open file upon creation
+  api.events.subscribe(api.events.Event.FileCreated, function(file)
+    vim.cmd("edit " .. file.fname)
+  end)
+end
+
 nvim_tree.setup {
+  on_attach = on_attach,
   update_focused_file = {
     enable = true,
     update_cwd = false,
@@ -50,14 +73,36 @@ nvim_tree.setup {
     },
   },
   view = {
-    width = 30,
-    side = "left",
-  },
+    float = {
+      enable = true,
+      open_win_config = function()
+        local screen_w = vim.opt.columns:get()
+        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+        local window_w = screen_w * WIDTH_RATIO
+        local window_h = screen_h * HEIGHT_RATIO
+        local window_w_int = math.floor(window_w)
+        local window_h_int = math.floor(window_h)
+        local center_x = (screen_w - window_w) / 2
+        local center_y = ((vim.opt.lines:get() - window_h) / 2)
+                         - vim.opt.cmdheight:get()
+        return {
+          border = 'rounded',
+          relative = 'editor',
+          row = center_y,
+          col = center_x,
+          width = window_w_int,
+          height = window_h_int,
+        }
+        end,
+    },
+    width = function()
+      return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+    end,
+  }
 }
 
 local opts = { noremap = true, silent = true }
-local keymap = vim.api.nvim_set_keymap
 
-keymap("n", "<leader>tt", ":NvimTreeToggle <CR>", opts)
-keymap("n", "<leader>tf", ":NvimTreeFindFileToggle <CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>tt", ":NvimTreeToggle <CR>", opts)
+vim.api.nvim_set_keymap("n", "<leader>tf", ":NvimTreeFindFileToggle <CR>", opts)
 
